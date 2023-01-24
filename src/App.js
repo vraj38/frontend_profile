fc31c16d63f5bf3ba17e76d38b9c21a8710bb81c
Vraj Patel <vrajpatel089@gmail.com>
2023-01-24 16:04:22 -0500

    Frontend Admin

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { MyProSidebarProvider } from "./pages/global/sidebar/sidebarContext";
+
+import Topbar from "./pages/global/Topbar";
+
+import Dashboard from "./pages/dashboard";
+import Team from "./pages/team";
+import Contacts from "./pages/contacts";
+import Line from "./pages/line";
+import Pie from "./pages/pie";
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyProSidebarProvider>
+          <div style={{ height: "100%", width: "100%" }}>
+            <main>
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+              </Routes>
+            </main>
+          </div>
+        </MyProSidebarProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;
